9a2ec0066e0a7f393faeb5c0ad783f14ff2bbcaa
manuel villagran <manuel.7000@hotmail.com>
2021-08-11 21:24:45 -0300

    Print some nicer error messages

--- src/bin/asm_compiler.rs
+++ src/bin/asm_compiler.rs
@@ -13,23 +13,39 @@ fn main() {
 	let filename = &args[1];
 
 	let input = fs::read_to_string(filename).expect("Unable to read file");
-	//Remove all blank lines
-	let input = input.replace("\n\n", "\n");
-	let input: Vec<Vec<&str>> = input
+	let input: Vec<Vec<String>> = input
 		.split("\n")
-		.map(|arg| arg.split(" ").collect())
+		.map(|arg| arg.split(" ").map(|arg| arg.replace("\r", "")).collect())
 		.collect();
 
+	let mut output: Vec<String> = Vec::new();
 	let opcodes = Instructions::new();
-	let mut output: String = String::new();
-	for instruction in input {
-		if instruction[0].is_empty() {
+	let mut line = 0;
+	for mut instruction in input {
+		line += 1;
+		if instruction[0].is_empty() || instruction[0].find("\r").unwrap_or(1) == 0 {
 			continue;
 		}
-		let instr = Instructions::get_instruction_from_name(instruction[0]).unwrap();
+		let instr = Instructions::get_instruction_from_name(instruction[0].as_str());
+		let instr = match instr {
+			Ok(i) => i,
+			Err(e) => {
+				println!("{} at line {}", e, line);
+				return;
+			}
+		};
+
+		instruction.retain(|x| x != "");
 		let arg_count = opcodes[&instr].arguments_count;
 		if instruction.len() != arg_count as usize + 1 {
-			panic!("Wrong number of arguments for instruction {}", instr);
+			println!(
+				"Wrong number of arguments for instruction {} at line {} (expected {} found {})",
+				instr,
+				line,
+				arg_count,
+				instruction.len() - 1
+			);
+			return;
 		}
 
 		let opc = instr.get_instruction_opc();
@@ -39,6 +55,7 @@ fn main() {
 		for i in 0..arg_count as usize {
 			let mut arg = instruction[i + 1].chars();
 			let first_char = arg.next().unwrap();
+			let arg: String = arg.collect();
 			let mut arg_string: String = String::new();
 			if first_char == '$' {
 				modes.push(ParameterModes::Immediate);
@@ -48,47 +65,52 @@ fn main() {
 				modes.push(ParameterModes::Position);
 				arg_string.push(first_char);
 			} else {
-				panic!("Not a numeric argument");
+				println!(
+					"{}{} isn't a numeric argument (line: {}, arg: {})",
+					first_char,
+					arg,
+					line,
+					i + 1
+				);
+				return;
 			}
-			let arg: String = arg.collect();
 			arg_string.push_str(arg.as_str());
 			args.push(arg_string);
 		}
 		modes.reverse();
 
+		let mut opcode_output = String::from("\n");
 		let mut found_1 = false;
 		for mode in modes {
 			match mode {
 				ParameterModes::Position => {
 					if found_1 {
-						output.push('0');
+						opcode_output.push('0');
 					}
 				}
 				ParameterModes::Immediate => {
-					output.push('1');
+					opcode_output.push('1');
 					found_1 = true;
 				}
 				ParameterModes::Relative => {
-					output.push('2');
+					opcode_output.push('2');
 					found_1 = true;
 				}
 				_ => panic!("Unknown mode"),
 			}
 		}
 		if opc < 10 && found_1 {
-			output.push('0');
+			opcode_output.push('0');
 		}
-		output.push_str(opc.to_string().as_str());
+		opcode_output.push_str(opc.to_string().as_str());
+		output.push(opcode_output);
 
 		for arg in args {
-			output.push(',');
-			output.push_str(arg.as_str());
+			output.push(arg);
 		}
-		output.push('\n');
 	}
-	output.pop();
 
-	let res = filename.find(".");
+	let res = filename[1..].find(".");
 	let output_name: String;
 	match res {
 		Some(i) => output_name = format!("{}.icc", String::from(&filename[0..i])),
@@ -98,7 +120,11 @@ fn main() {
 	let file = fs::File::create(&output_name);
 	let mut file = match file {
 		Ok(f) => f,
-		Err(e) => panic!("Problem creating file {}: {}", output_name, e),
+		Err(e) => {
+			println!("Problem creating file {}: {}", output_name, e);
+			return;
+		}
 	};
-	file.write(output.as_bytes()).unwrap();
+	file.write(output.join(",").trim_start().as_bytes())
+		.unwrap();
 }
